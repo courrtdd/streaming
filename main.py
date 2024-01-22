593ecc03e8f4ff0acc6d3e45a63c76166d07919c
courrtdd <155004207+courrtdd@users.noreply.github.com>
2024-01-22 19:51:23 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+os.system("pip install discord.py==1.7.3")
+import discord, asyncio, datetime, pytz,keepalive
+
+
+from discord.ext import tasks, commands
+
+client = commands.Bot(
+  command_prefix='+',
+  self_bot=True
+)
+
+
+
+@client.event
+async def on_connect():
+  await client.change_presence(activity = discord.Streaming(name = "sam", url = "https://www.twitch.tv/#"))
+
+
+keepalive.keepalive()
+client.run(os.getenv("MTE0NTY4NjQ2NjI0OTM3MTczOQ.GIyWpY.PApcxncvQnPstcKmzCpdfUMRHJyiPVwKZIylf0"), bot=False)
+#Now press f5 to run the project
+#credit of project goes to me 
